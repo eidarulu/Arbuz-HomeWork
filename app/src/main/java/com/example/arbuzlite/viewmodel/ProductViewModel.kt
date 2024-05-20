cef8f9582af6b85d44e58195b785d541c9eccbaa
Yeldos <eldos.amire@gmail.com>
2024-05-21 01:20:23 +0500

    Implemented the ProductViewModel

--- app/src/main/java/com/example/arbuzlite/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/arbuzlite/viewmodel/ProductViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.arbuzlite.viewmodel
 
-class ProductViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.arbuzlite.data.BasketItem
+import com.example.arbuzlite.data.Product
+import com.example.arbuzlite.data.ProductRepository
+import kotlinx.coroutines.launch
+
+class ProductViewModel(private val repository: ProductRepository) : ViewModel() {
+
+    val products = liveData {
+        emit(repository.getAllProducts())
+    }
+
+    val basketItems = liveData {
+        emit(repository.getBasketItems())
+    }
+
+    fun insertProduct(product: Product) = viewModelScope.launch {
+        repository.insertProduct(product)
+    }
+
+    fun insertBasketItem(basketItem: BasketItem) = viewModelScope.launch {
+        repository.insertBasketItem(basketItem)
+    }
 }
